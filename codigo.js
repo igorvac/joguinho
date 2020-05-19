1c992a7eb7ec705fe5f652f3844d50d017ba2594
igorvac <igorvac@gmail.com>
2020-05-19 13:28:21 -0300

    v1

--- codigo.js
+++ codigo.js
@@ -4,8 +4,8 @@
 
 // Características da moeda
 
-let moedaX = 0; // Posição X da moeda
-let moedaY = 0; // Posição Y da moeda
+let moedaX = 0; // Posição horizontal da moeda
+let moedaY = 0; // Posição vertical da moeda
 let moedaTamanho = 30; // Tamanho da moeda
 let velo = 2; // Velocidade de queda da moeda
 let moedaImg; // Vai armazenar a imagem da moeda
@@ -34,39 +34,37 @@ function setup() {
 }
 
 function draw() { // Aqui começa a desenhar na tela
+  
   distCheck(); // Checa a distancia entre a moeda e a personagem
 
-  //background(0, 0, 0);
-  image(fundoImg, width/2, height/2, width, height);
+  image(fundoImg, width/2, height/2, width, height); //Desenha a imagem do fundo
 
-  fill(255, 255, 255);
-  textAlign(LEFT);
-  textSize(30);
-  text(pontos, 400, 50)
+  fill(255, 255, 255); // Cor do texto em branco
+  textSize(30); // Tamanho do texto em 30 pixels
+  text(pontos, 400, 50) // Desenha o texto na tela com os pontos
 
-  //fill(255, 255, 0);
-  //ellipse(moedaX, moedaY, moedaTamanho, moedaTamanho);
-  image(moedaImg, moedaX, moedaY, moedaTamanho, moedaTamanho);
+  // Desenha a imagem
+  image(moedaImg, moedaX, moedaY, moedaTamanho, moedaTamanho); 
+  moedaY += velo; // Soma a velocidade com a posição da moeda na tela
 
-  //fill(255, 255, 255);
-  //ellipse(mouseX, height - personagemTamanho / 2, personagemTamanho, personagemTamanho);
-  image(personagemImg, mouseX, height - personagemTamanho , personagemTamanho, personagemTamanho);
+  // Desenha a personagem 
+  image(personagemImg, mouseX, height - personagemTamanho , personagemTamanho, personagemTamanho); 
 
+  
+  //Aqui temos duas condições: Se a moeda não encostar no personagem e se a moeda encostar no personagem
 
-  moedaY += velo;
-
-  if (moedaY > height) {
-    moedaY = -moedaTamanho;
-    moedaX = random(moedaTamanho / 2, width - moedaTamanho / 2);
-    if (pontos > 0) {
-      pontos = pontos - 1;
+  if (moedaY > height) {  //Se a moeda NÃO encostar
+    moedaY = 0; // Desenha a moeda no topo da tela
+    moedaX = random(moedaTamanho / 2, width - moedaTamanho / 2); // Sorteia a nova posição horizontal da moeda
+    if (pontos > 0) { // Se ele tiver mais pontos que 0
+      pontos = pontos - 1; // Remover um ponto
     }
   }
 
-  if (distance <= moedaTamanho + personagemTamanho) {
-    pontos = pontos + 1;
-    moedaY = -moedaTamanho;
-    moedaX = random(moedaTamanho / 2, width - moedaTamanho / 2);
+  if (distance <= moedaTamanho + personagemTamanho) { //Se a moeda encostar
+    pontos = pontos + 1; // Soma um ponto
+    moedaY = 0; // Desenha a moeda no topo da tela
+    moedaX = random(moedaTamanho / 2, width - moedaTamanho / 2); // Sorteia a nova posição horizontal da moeda
   }
 }
 
